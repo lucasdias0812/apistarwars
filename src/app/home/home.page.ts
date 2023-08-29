51a784043d4a660ca8bbd39650ebefe0df8c0654
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2023-08-29 12:53:23 -0300

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-
+import {Router} from '@angular/router';
+import {Observable, catchError} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-  films: Observable<any> | undefined
 
-  constructor(private router: Router, private http: HttpClient) {}
+export class HomePage implements OnInit {
+  films: Observable<any> | undefined; //Para arrumar o erro da definição do films, devido a atualização do Ionic que obriga a ter um valor de início
+  constructor(private router: Router, private http: HttpClient, public toastController: ToastController) {}
 
-  ngOnInit() {
-    this.films = this.http.get('https://swapi.dev/api/films')
+  ngOnInit(){
 
+    this.films = this.http.get('https://swapi.dev/api/films').pipe(
+      catchError(erro => this.exibirErro(erro))
+    );
   }
 
-  openDetails(film) {
+  async exibirErro(erro:any){
+
+    const toast = await this.toastController.create({
+      message: 'Erro ao consultar API: ' + erro.status + ':' + erro.message,
+      duration: 4000,
+      color: 'danger',
+      position: 'middle'
+    });
+    console.log(erro);
+    toast.present();
+    return null;
+  }
+  openDetails(film:any){
     let split = film.url.split('/');
-    let filmId = split[split.lenght-2];
-    this.router.navigateByUrl(`/filme-detalhe/${filmId}`)
+    let filmId = split[split.length-2];
+    this.router.navigateByUrl('/filme-detalhe/${filmId}');
   }
 }
